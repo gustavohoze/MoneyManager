25afdc2f18cfcd7755f69e39ce9bc5c9dd337396
gustavohoze <114294109+gustavohoze@users.noreply.github.com>
2026-03-16 17:49:27 +0700

    Initial Commit

---
+++ MoneyManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoneyManager
+//
+//  Created by Gustavo Hoze on 16/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoneyManager/MoneyManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyManagerApp.swift
+//  MoneyManager
+//
+//  Created by Gustavo Hoze on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MoneyManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
